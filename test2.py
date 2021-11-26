615c1336e1ecd36a23ae673a6431f0bdf8694e59
Ssslakter <chauninslava2003@gmail.com>
2021-11-26 22:46:03 +0700

    test. deleted spaces.

--- test2.py
+++ test2.py
@@ -1,23 +1,16 @@
 from tkinter import *
 import pygame
 
-
 root = Tk()
 root.title("Tuturu")
 root.geometry("500x300")
 
 pygame.mixer.init()
 
-
-
-
+print("abc")
 def play():
     pygame.mixer.music.load('D:\\Python code\\meme_data\\tuturu\\qq.mp3')
     pygame.mixer.music.play()
-
-
-
-
 
 btn = Button(text="Tuturu ",          # текст кнопки
              background="#c3c11c",     # фоновый цвет кнопки
